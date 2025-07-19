fafc0b5c0cbe36c6fa673606dcbc70c893b0f686
B-Beetroot <216270728+B-Beetroot@users.noreply.github.com>
2025-07-19 02:18:24 +0200

    Add UI rps game

--- rps.js
+++ rps.js
@@ -1,5 +1,4 @@
-//Create a new function named getComputerChoice.
-//return one of the following string values: “rock”, “paper” or “scissors”.
+
 function getComputerChoice(){
     let randomChoice = Math.floor(Math.random()*100);
     if (randomChoice <33) {
@@ -11,65 +10,82 @@ function getComputerChoice(){
     }   
 }
 
-//Create a new function named getHumanChoice.
-//getHumanChoice will return one of the valid choices depending on what the user inputs. 
-function getHumanChoice(){
-    return prompt("Enter your choice");
-}
-
-//write variables to keep track of the players score.
-//two new variables named humanScore and computerScore in the global scope. initialize 0.
-
-//new function named playRound.
-//two parameters for playRound: humanChoice and computerChoice
-//function’s humanChoice parameter case-insensitive
-//Increment the humanScore or computerScore variable based on the round winner.
-
-// playRound function to console.log a string value representing the round winner.
-//game will play 5 rounds. function named playGame that calls playRound to play 5 rounds
-//Move your playRound function and score variables so that they’re declared inside of the new playGame function
 function playGame (){
 
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
+    function playRound(humanChoice, computerChoice) {
 
-humanChoice = getHumanChoice()?.toLocaleLowerCase();
-computerChoice = getComputerChoice();
+    const pChoice = document.querySelector("#pChoice");
+    const cChoice = document.querySelector("#cChoice");
 
-console.log("Computer Choice = " + computerChoice);
-console.log("Your Choice = " + humanChoice);
+    pChoice.textContent = "Your Choice: " + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+    cChoice.textContent = "Computer Choice: " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);  
+    }   
 
-if (humanChoice === computerChoice) {
-        return "It's a tie!";
-    } else if (
-        (humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice === "paper" && computerChoice === "rock") ||
-        (humanChoice === "scissors" && computerChoice === "paper")
-    ) {
-        humanScore++;
-        return "You win!";
-    } else {
-        computerScore++;
-        return "Computer wins!";
-    }
+    const infoHuman = document.querySelector("#infoHuman");
+    const infoMain = document.querySelector("#infoMain");
+    const infoComp = document.querySelector("#infoComp");
+    const buttons = document.querySelectorAll('button');
 
+    buttons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            humanChoice = event.currentTarget.id;
+            computerChoice = getComputerChoice();
+            
+            playRound(humanChoice, computerChoice);
+
+                if (humanChoice === computerChoice) {
+                    infoMain.textContent = "It's a Tie!";
+                    } else if (
+                        (humanChoice === "rock" && computerChoice === "scissors") ||
+                        (humanChoice === "paper" && computerChoice === "rock") ||
+                        (humanChoice === "scissors" && computerChoice === "paper")
+                    ) {
+                        ++humanScore;
+                        infoMain.textContent = "You win!";
+                        infoHuman.textContent = "Your score: " + humanScore;
+                    } else {
+                        ++computerScore;
+                        infoMain.textContent = "Computer wins!";
+                        infoComp.textContent = "Computer score: " + computerScore;
+                    }
+        
+            if (computerScore === 5 || humanScore === 5) {
+                infoMain.textContent = humanScore === 5 ? "Game Over: You Win!" : "Game Over: Computer Wins!";
+
+                humanScore = 0;
+                computerScore = 0;
+
+                const restart = document.createElement("button");
+                const restartImg = document.createElement("img");
+                restartImg.src = "./images/restart.png";         
+                restartImg.alt = "Restart";                     
+                restartImg.title = "Restart";                    
+                restartImg.style.width = "50%";               
+                restartImg.style.height = "auto";                
+                restart.appendChild(restartImg);
+
+                const playButtons = document.querySelector("#playButtons");
+                buttons.forEach(button => {
+                    button.style.display = "none";
+                });
+                playButtons.appendChild(restart);
+                restart.addEventListener("click", () => {
+                buttons.forEach(button => {
+                    button.style.display = "inline-block";
+                });
+                infoHuman.textContent = "Your Score: 0";
+                infoComp.textContent = "Computer Score: 0";
+                infoMain.textContent = "Make a Choice";
+                pChoice.textContent = "";
+                cChoice.textContent = "";
+                restart.remove(); 
+                });
+                }
+            });
+        });
 }
-
-
-for (i=1; i<6 ; i++) {
-console.log("Round " + i + " - " + playRound() + " - Your score: " + humanScore + " - Computer score: " + computerScore);
-}
-if (humanScore > computerScore){
-console.log("Game Winner => You Win!" + " - Total score => You: " + humanScore + " - Computer: " + computerScore);
-} else if (humanScore < computerScore) {
-console.log("Game Winner => Computer Wins!" + " - Total score: => You: " + humanScore + " - Computer: " + computerScore);
-} else {
-    console.log("Game Winner => It's a tie!" + " - Total score: => You: " + humanScore + " - Computer: " + computerScore);
-}
-
-}
-
 
 playGame();
